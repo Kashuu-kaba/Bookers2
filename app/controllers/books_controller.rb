9c85d8876a7d062241d687d0c3fb4076d7e4c8e3
Kashuu-kaba <Kaasyuu@outlook.jp>
2021-05-31 06:33:30 +0000

    5/31 commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,24 @@
 class BooksController < ApplicationController
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @user = @book.user
-    if @book.save(book_params)
-      redirect_to book_path(@book.id), notice: "create successfully"
-    else
-      @books = Book.all
-      render :index
-    end
-  end
-
   def index
-    @book = Book.new(create_params)
+    @book = Book.new()
     @user = current_user
     @books = Book.all
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "create successfully"
+    else
+      @books = Book.all
+      render "index"
+    end
+  end
 
   def show
-    @book = Book.new()
-    @b = Book.find(params[:id])
-    @user = @b.user
+    @book_new = Book.new()
+    @book = Book.find(params[:id])
     @book_comment = BookComment.new()
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#top"
-
   get "home/about" => "home#about"
-
   resources :books, except: [:new] do
     resource :favorites, only: [:create, :destroy]
 
